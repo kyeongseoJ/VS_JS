ab933b3cc406f89068332342a4d5bb2de04476c7
kyeongseoJ <kyeongseojeon@gmail.com>
2021-12-12 18:09:59 +0900

    데이터백업

---
+++ js/1210_ex002.js
@@ -0,0 +1,20 @@
+// variable 변수  각각 선언하기
+var pred;
+var input_x;
+var weight;
+var bias;
+
+// 한줄로 변수 여러개 선언하기
+var pred, input_x, weight, bias;
+
+// 변수선언과 값 할당 따로 하기
+var input_x;
+input_x = 2022;
+
+// 변수 선언과 값 할당 같이 하기
+var weigjt = 0.123;
+var bias = 0.0;
+
+
+
+
---
+++ js/1210_ex003.js
@@ -0,0 +1,10 @@
+// 변수 할당
+var input = 3;
+// var output = "y";
+
+// 변수 값 할당됬는지 확인
+console.log(input);
+// 변수의 자료형 타입 확인하기
+console.log(typeof("input"));
+
+
