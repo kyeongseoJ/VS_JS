51babaf935718eae7b88694c2d1ef3cc2a987a02
kyeongseoJ <kyeongseojeon@gmail.com>
2022-01-13 19:33:33 +0900

    0113 add commit2

--- momentum/app.js
+++ momentum/app.js
@@ -39,23 +39,28 @@ const { flatten } = require("@tensorflow/tfjs-layers/dist/exports_layers");
 // }else {
 //     console.log("Thank you fot writing your age.")
 // }
-const age = parseInt(prompt("How old are you?"));
+// const age = parseInt(prompt("How old are you?"));
 
-if (isNaN(age) || age < 0) {
-    console.log("Pls write a real positive number.");
-}else if(age < 18){
-    console.log("You are too young.")
-} else if(age >= 18 && age <= 50){
-    console.log("You can drink.")
-} else if(age > 50 && age <= 80){
-    console.log("You should exercise.")
-}
-else {
-    console.log("You can do whatever you want.")
-}
+// if (isNaN(age) || age < 0) {
+//     console.log("Pls write a real positive number.");
+// }else if(age < 18){
+//     console.log("You are too young.")
+// } else if(age >= 18 && age <= 50){
+//     console.log("You can drink.")
+// } else if(age > 50 && age <= 80){
+//     console.log("You should exercise.")
+// }
+// else {
+//     console.log("You can do whatever you want.")
+// }
 
-true || true === true
-true || false === false
-false || true === false
-false || false === false
+// true || true === true
+// true || false === true
+// false || true === true
+// false || false === false
 
+const hi = document.getElementById("hi")
+
+hi.innerText = "Hey, Hi.";
+
+console.log(hi.className);
