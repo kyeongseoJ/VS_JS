9e8fdb71ef2541d520f0b1366501b1f51e4873fd
kyeongseoJ <kyeongseojeon@gmail.com>
2022-01-24 22:13:04 +0900

    0124 add5

--- 유정이설명/twitch/twitch.js
+++ 유정이설명/twitch/twitch.js
@@ -1,26 +1,26 @@
-const toggleIcon = $('.toggle_icon')
-const openIcon = $('.open_icon')
-const closeIcon = $('.close_icon')
+const toggleIcon = document.querySelector('.toggle_icon')
+const openIcon = document.querySelector('.open_icon')
+const closeIcon = document.querySelector('.close_icon')
 
 
-toggleIcon.on('click', () => {
+toggleIcon.addEventListener('click', () => {
     console.log('cklicked')
-    $('.menu_container').toggle('show')
+    document.querySelector('.menu_container').toggle('show')
 })
 
-openIcon.on('click', () => {
-    console.log('open')
-    $('.nav').addClass('show_nav')
-    $('.nav_icons').addClass('static')
-    $('.open').addClass('none')
-    $('.close').addClass('flex')
-})
-
-closeIcon.on('click', () => {
+openIcon.addEventListener('click', () => {
     console.log('close')
-    $('.nav').removeClass('show_nav')
-    $('.nav_icons').removeClass('static')
-    $('.open').removeClass('none')
-    $('.close').removeClass('flex')
+    document.querySelector('.nav').classList.add('show_nav')
+    document.querySelector('.nav_icons').classList.add('static')
+    document.querySelector('.follow_channel').classList.add('none')
+    document.querySelector('.close').classList.add('flex')
+})
+
+closeIcon.addEventListener('click', () => {
+    console.log('open')
+    document.querySelector('.nav').classList.remove('show_nav')
+    document.querySelector('.nav_icons').classList.remove('static')
+    document.querySelector('.follow_channel').classList.remove('none')
+    document.querySelector('.close').classList.remove('flex')
 })
 
