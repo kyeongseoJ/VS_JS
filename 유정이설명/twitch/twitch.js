b9f6b6e04e8141023b51b6f8d5a2bb57b7399f00
kyeongseoJ <kyeongseojeon@gmail.com>
2022-01-24 21:45:15 +0900

    0124 add4

--- 유정이설명/twitch/twitch.js
+++ 유정이설명/twitch/twitch.js
@@ -1,6 +1,26 @@
 const toggleIcon = $('.toggle_icon')
+const openIcon = $('.open_icon')
+const closeIcon = $('.close_icon')
+
 
 toggleIcon.on('click', () => {
     console.log('cklicked')
     $('.menu_container').toggle('show')
 })
+
+openIcon.on('click', () => {
+    console.log('open')
+    $('.nav').addClass('show_nav')
+    $('.nav_icons').addClass('static')
+    $('.open').addClass('none')
+    $('.close').addClass('flex')
+})
+
+closeIcon.on('click', () => {
+    console.log('close')
+    $('.nav').removeClass('show_nav')
+    $('.nav_icons').removeClass('static')
+    $('.open').removeClass('none')
+    $('.close').removeClass('flex')
+})
+
